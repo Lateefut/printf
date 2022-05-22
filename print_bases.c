cabf117e096483f6ed385356b723dff64a3e394c
Lateefut <adebanjolateef18@gmail.com>
2022-05-22 01:50:18 -0700

    all about hexes

--- print_bases.c
+++ print_bases.c
@@ -37,7 +37,7 @@ int print_hex_big(va_list l, flags_t *f)
 	unsigned int num = va_arg(l, unsigned int);
 	char *str = convert(num, 16, 0);
 	int count = 0;
-	
+
 	if (f->hash == 1 && str[0] != '0')
 		count += _puts("0X");
 	count += _puts(str);
@@ -57,7 +57,7 @@ int print_binary(va_list l, flags_t *f)
 {
 	unsigned int num = va_arg(l, unsigned int);
 	char *str = convert(num, 2, 0);
-	
+
 	(void)f;
 	return (_puts(str));
 }
@@ -76,7 +76,7 @@ int print_octal(va_list l, flags_t *f)
 	unsigned int num = va_arg(l, unsigned int);
 	char *str = convert(num, 8, 0);
 	int count = 0;
-	
+
 	if (f->hash == 1 && str[0] != '0')
 		count += _putchar('0');
 	count += _puts(str);
